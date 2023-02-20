41a9909a19a6619c591a58a3bb3371f70300c6a1
Bagus Miftaq Hurohman <bagusmiftaq@gmail.com>
2023-02-20 13:46:36 +0700

    Integrasi Backend

--- src/pages/MenuList/MenuItem/index.js
+++ src/pages/MenuList/MenuItem/index.js
@@ -1,18 +1,31 @@
 import {StyledListItem} from "./styles";
 import {Button, ButtonGroup, Col} from "react-bootstrap";
 import React from "react";
+import {motion} from "framer-motion";
 
 const MenuItem = ({data, onDelete}) => {
     return(
-        <StyledListItem action>
-            <Col>
-                <h3 className="font-weight-bold">{data?.title}</h3>
-                <p>Rp. {Number(data?.price).toLocaleString("id-ID")}</p>
-            </Col>
+        <motion.div
+        initial={
+            {
+                x : 100,
+                y:200
+            }
+        }
+            animate={{
+                x: 0,
+                y:0
+            }}>
+            <StyledListItem action >
+                <Col>
+                    <h3 className="font-weight-bold">{data?.title}</h3>
+                    <p>Rp. {Number(data?.price).toLocaleString("id-ID")}</p>
+                </Col>
                 <ButtonGroup>
                     <Button variant="outline-danger" style={{width: "100px"}} onClick={onDelete}>Delete</Button>
                 </ButtonGroup>
-        </StyledListItem>
+            </StyledListItem>
+        </motion.div>
     )
 }
 
